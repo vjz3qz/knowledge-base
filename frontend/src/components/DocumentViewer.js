d969bb299dc8167129c8bb58cb28077a31a210d5
vjz3qz <vjz3qz@virginia.edu>
2023-11-22 15:35:53 -0500

    included api call to backend

--- frontend/src/components/DocumentViewer.js
+++ frontend/src/components/DocumentViewer.js
@@ -22,20 +22,20 @@ function DocumentViewer({ id }) {
       setMetadata(result.data);
     };
     try {
-    //   fetchFile();
-    //   fetchMetadata();
-    setFileUrl("https://www.clickdimensions.com/links/TestPDFfile.pdf");
-    setMetadata({
-        "file_name": "sample.pdf",
-        "file_type": "text",
-        "summary": "This is a sample summary.",
-        "classification_data": {
-            "class_counts": {
-                "class1": 1,
-                "class2": 2
-            }
-        }
-    });
+      fetchFile();
+      fetchMetadata();
+    // setFileUrl("https://www.clickdimensions.com/links/TestPDFfile.pdf");
+    // setMetadata({
+    //     "file_name": "sample.pdf",
+    //     "file_type": "text",
+    //     "summary": "This is a sample summary.",
+    //     "classification_data": {
+    //         "class_counts": {
+    //             "class1": 1,
+    //             "class2": 2
+    //         }
+    //     }
+    // });
     } catch (error) {
       console.log(error);
     }
--- frontend/src/utils/GetSearchResults.js
+++ frontend/src/utils/GetSearchResults.js
@@ -1,39 +1,41 @@
+// import axios
+import axios from "axios";
 async function getSearchResults(searchTerm) {
   const payload = {
     query: searchTerm,
   };
-//   const result = await axios.post(
-//     "http://localhost:5001/api/v2/search/3",
-//     payload
-//   );
+  const result = await axios.post(
+    "http://localhost:5001/api/v2/search/3",
+    payload
+  );
 
-  const result = {
-    data: {
-      answer: "The impact of climate change on marine life is significant.",
-      sources: {
-        source1: {
-          metadata: {
-            file_type: "pdf",
-            content_type: "research_paper",
-            name: "Impact of Climate Change on Marine Life",
-            summary:
-              "This research paper discusses the various impacts of climate change on marine biodiversity, including rising sea temperatures and ocean acidification.",
-            url: "http://example.com/research/climate_change_marine_life.pdf",
-          },
-        },
-        source2: {
-          metadata: {
-            file_type: "image",
-            content_type: "photograph",
-            name: "Aurora Borealis in Norway",
-            summary:
-              "A breathtaking photograph capturing the Aurora Borealis over the Norwegian landscape during the winter season.",
-            url: "http://example.com/images/aurora_borealis_norway.jpg",
-          },
-        },
-      },
-    },
-  };
+  // const result = {
+  //   data: {
+  //     answer: "The impact of climate change on marine life is significant.",
+  //     sources: {
+  //       source1: {
+  //         metadata: {
+  //           file_type: "pdf",
+  //           content_type: "research_paper",
+  //           name: "Impact of Climate Change on Marine Life",
+  //           summary:
+  //             "This research paper discusses the various impacts of climate change on marine biodiversity, including rising sea temperatures and ocean acidification.",
+  //           url: "http://example.com/research/climate_change_marine_life.pdf",
+  //         },
+  //       },
+  //       source2: {
+  //         metadata: {
+  //           file_type: "image",
+  //           content_type: "photograph",
+  //           name: "Aurora Borealis in Norway",
+  //           summary:
+  //             "A breathtaking photograph capturing the Aurora Borealis over the Norwegian landscape during the winter season.",
+  //           url: "http://example.com/images/aurora_borealis_norway.jpg",
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
 
   // TODO add this to the backend
   const data = result.data;
