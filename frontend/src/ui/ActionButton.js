2287c9c34652fa386b04008369a515d6dd8434dc
vjz3qz <vjz3qz@virginia.edu>
2023-11-23 22:26:49 -0500

    disabled action button style

--- frontend/src/ui/ActionButton.js
+++ frontend/src/ui/ActionButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const ActionButton = ({ onClick, highlight, label }) => {
-  const buttonClass = highlight ? 'action-button-black' : 'action-button-white';
+const ActionButton = ({ onClick, highlight, label, disabled }) => {
+  let buttonClass = 'action-button-white';
+  if (highlight) {
+    buttonClass = 'action-button-black';
+  }
+  if (disabled) {
+    buttonClass += ' disabled'; // Assuming you have appropriate styles for the disabled state
+  }
 
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
       {label}
     </button>
   );
