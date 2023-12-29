e12bf1ac06046cac0c42446b2bc0d4fa61386e3c
vjz3qz <vjz3qz@virginia.edu>
2023-12-29 18:42:56 -0500

    developed interface

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -3,28 +3,20 @@ from . import v1
 from flask import request, jsonify
 
 
-@v1.route('/search', methods=['GET'])
-def search():
+@v1.route('/search-component', methods=['GET'])
+def search_component():
     """
-    Endpoint for searching the Chroma database.
+    Endpoint for searching for a component.
 
     Parameters:
     query (str): The search string.
-    mediaType (str): Filter by media type (note, document, diagram, video).
-    contentType (str): Filter by content type.
-    tags (str): Filter by tags (safety, maintenance, training, etc.).
-    componentList (str): Filter by components listed in the media.
 
     Returns:
-    list: A list of media items that match the search criteria, including IDs, summaries, and other relevant metadata.
+    list: A list of component items that match the search criteria, including IDs, summaries, and other relevant metadata.
     """
     query = request.args.get('query', '')
-    media_type = request.args.get('mediaType', '')
-    content_type = request.args.get('contentType', '')
-    tags = request.args.get('tags', '')
-    component_list = request.args.get('componentList', '')
     try:
-        results = combined_search_media(query, media_type, content_type, tags, component_list)
+        results = component_search(query)
     except Exception as e:
         # Optionally log the exception (not shown here)
         print(e)
@@ -32,49 +24,76 @@ def search():
     return jsonify(results)
 
 
-@v1.route('/get-viewing-session', methods=['GET'])
-def get_viewing_session():
+@v1.route('/search-media', methods=['GET'])
+def search_media():
     """
-    Endpoint for getting a viewing session.
+    Endpoint for searching the media/history associated with a component.
 
     Parameters:
-    id (str): The unique identifier of the user.
+    query (str): The search string.
+    component_id (str): The ID of the component to search.
 
     Returns:
-    JSON: A JSON object containing the ID array of the user's viewing session.
+    list: A list of media items that match the search criteria, including IDs, summaries, and other relevant metadata.
     """
-    id = request.args.get('id', '')
+    query = request.args.get('query', '')
+    component_id = request.args.get('component_id', '')
     try:
-        viewing_session = get_viewing_session_by_id(id)
+        results = combined_search_media(query, component_id)
+    except Exception as e:
+        # Optionally log the exception (not shown here)
+        print(e)
+        return jsonify({"error": "Server error"}), 500
+    return jsonify(results)
+
+
+@v1.route('/view-media', methods=['GET'])
+def view_media():
+    """
+    Endpoint for viewing a media object.
+
+    Parameters:
+    media_id: media ID for the viewing session.
+
+    Returns:
+    JSON: A JSON object containing the description and the title of the media object.
+    """
+    media_id = request.args.get('media_id', '')
+    try:
+        viewing_session = fetch_media_object(media_id)
     except Exception as e:
         # Optionally log the exception (not shown here)
         print(e)
         return jsonify({"error": "Server error"}), 500
     return jsonify(viewing_session)
+    
 
-@v1.route('/add-viewing-session', methods=['POST'])
-def add_viewing_session():
+@v1.route('/create-component', methods=['POST'])
+def create_component():
     """
-    Endpoint for adding a viewing session.
+    Endpoint for creating a new component item.
 
     Parameters:
-    userId (str): Identifier for the user.
-    documentID (str): Document ID to add to the user's viewing session.
+    author (str): Author of the component.
+    title (str): Title of the component.
+    description (str): Actual content or summary.
+    media_list (str): media associated with the component.
 
-    Action:
-    Saves the current state of the user's viewing session in the database.
+    Returns:
+    JSON: A JSON object containing the confirmation of creation, including the hash of the new component item.
     """
-    user_id = request.json['userId']
-    document_id = request.json['documentID']
+    author = request.json['author']
+    title = request.json['title']
+    description = request.json['description']
+    media_list = request.json['media_list']
     try:
-        add_document_id_to_user_viewing_session(user_id, document_id)
+        media_id = create_new_component(author, title, description, media_list)
     except Exception as e:
         # Optionally log the exception (not shown here)
         print(e)
         return jsonify({"error": "Server error"}), 500
-    return jsonify({"success": True})
-
-
+    return jsonify(media_id)
+    
 
 @v1.route('/create-media', methods=['POST'])
 def create_media():
@@ -82,28 +101,24 @@ def create_media():
     Endpoint for creating a new media item.
 
     Parameters:
-    mediaType (str): Type of media being created (note, document, diagram, video).
-    contentType (str): Content type of the media.
     author (str): Author of the media.
+    title (str): Title of the media.
     description (str): Actual content or summary.
-    tags (str): Tags associated with the media.
-    componentList (str): Components associated with the media.
-    permissions (str): Access permissions.
+    component_id (str): Component associated with the media.
+
+    TODO
     files (str): File data for images/videos/documents (to be stored in S3).
 
     Returns:
     JSON: A JSON object containing the confirmation of creation, including the ID of the new media item.
     """
-    media_type = request.json['mediaType']
-    content_type = request.json['contentType']
     author = request.json['author']
+    title = request.json['title']
     description = request.json['description']
-    tags = request.json['tags']
-    component_list = request.json['componentList']
-    permissions = request.json['permissions']
-    files = request.json['files']
+    component_id = request.json['component_id']
+    # files = request.json['files']
     try:
-        media_id = create_new_media(media_type, content_type, author, description, tags, component_list, permissions, files)
+        media_id = create_new_media(author, title, description, component_id)
     except Exception as e:
         # Optionally log the exception (not shown here)
         print(e)
@@ -129,3 +144,15 @@ def assistant_chat():
         print(e)
         return jsonify({"error": "Server error"}), 500
     return jsonify(assistant_response)
+
+
+
+@v1.route('/test_db')
+def test_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM media LIMIT 1;')  # Replace with your table name
+    data = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return str(data)
