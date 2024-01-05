c6fc913386b78efc8f1165011bda70d3e065207e
vjz3qz <vjz3qz@virginia.edu>
2024-01-05 18:02:47 -0500

    finished search media by component id

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -5,6 +5,7 @@ from langchain_community.vectorstores import SupabaseVectorStore
 from supabase.client import Client, create_client
 import os
 
+
 # TODO check if all supabase logic is correct
 def init_db():
     """
@@ -13,59 +14,108 @@ def init_db():
     supabase_url = os.environ.get("SUPABASE_URL")
     supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
     supabase: Client = create_client(supabase_url, supabase_key)
-    return supabase   
+    return supabase
 
 
-supabase = init_db() 
+supabase = init_db()
 
 
 def search_components(query):
-    data, count = supabase.table('component').select('*').text_search('text_search_vector', query).execute()
+    data, count = (
+        supabase.table("components")
+        .select("*")
+        .text_search("text_search_vector", query)
+        .execute()
+    )
     results = data[1]
     return results
 
 
-def search_media(query):
-    data, count = supabase.table('media').select('*').text_search('text_search_vector', query).execute()
+def search_media(query, component_id):
+    data, count = (
+        supabase.table("media")
+        .select("*")
+        .join(
+            "component_media_association",
+            "media.id",
+            "component_media_association.media_id",
+        )
+        .eq("component-media-association.component_id", component_id)
+        .text_search("text_search_vector", query)
+        .execute()
+    )
     results = data[1]
     return results
 
 
 def create_component(component_id, title, description, location):
-    data, count = supabase.table('components').insert({'id': component_id,
-                                                      'title': title,
-                                                      'description': description,
-                                                      'location': location,
-                                                      }).execute()
+    data, count = (
+        supabase.table("components")
+        .insert(
+            {
+                "id": component_id,
+                "title": title,
+                "description": description,
+                "location": location,
+            }
+        )
+        .execute()
+    )
 
 
 def create_media(media_id, author, title, description, component_ids):
-    data, count = supabase.table('media').insert({'id': media_id,
-                                                  'author': author,
-                                                  'title': title,
-                                                  'description': description,
-                                                  'image_ids': [],
-                                                  'video_ids': [],
-                                                  'document_ids': [],
-                                                  }).execute()
+    data, count = (
+        supabase.table("media")
+        .insert(
+            {
+                "id": media_id,
+                "author": author,
+                "title": title,
+                "description": description,
+                "image_ids": [],
+                "video_ids": [],
+                "document_ids": [],
+            }
+        )
+        .execute()
+    )
     for component_id in component_ids:
-        data, count = supabase.table('component_media_association').insert({'media_id': media_id,
-                                                                            'component_id': component_id,
-                                                                            }).execute()
+        data, count = (
+            supabase.table("component_media_association")
+            .insert(
+                {
+                    "media_id": media_id,
+                    "component_id": component_id,
+                }
+            )
+            .execute()
+        )
 
 
 def add_file_to_media(file_id, file_type, media_id):
     # add file_id to media_id's corresponding file_type array in supabase
-    original_data = supabase.table('media').select(f'{file_type}_ids').eq('id', media_id).execute().data
-    updated_data, count = (supabase.table('media')
-    .update({file_type+'_ids': original_data.append(file_id)})
-    .eq('id', media_id)
-    .execute())
+    original_data = (
+        supabase.table("media")
+        .select(f"{file_type}_ids")
+        .eq("id", media_id)
+        .execute()
+        .data
+    )
+    updated_data, count = (
+        supabase.table("media")
+        .update({file_type + "_ids": original_data.append(file_id)})
+        .eq("id", media_id)
+        .execute()
+    )
     # split file into chunks
     docs = load_and_split(file_id, media_id, file_type)
-    # set appropriate metadata for each chunk 
+    # set appropriate metadata for each chunk
     for doc in docs:
-        doc.metadata = { 'file_id': file_id, 'file_type': file_type, 'media_id': media_id }
+        doc.metadata = {
+            "file_id": file_id,
+            "file_type": file_type,
+            "media_id": media_id,
+        }
     # create embedding for file and add to supabase
     vector_store = SupabaseVectorStore.from_documents(
         docs,
@@ -79,13 +129,14 @@ def add_file_to_media(file_id, file_type, media_id):
 
 
 def get_component_by_id(component_id):
-    response = supabase.table('components').select('*').eq('id', component_id).execute()
+    response = supabase.table("components").select("*").eq("id", component_id).execute()
     return response
 
 
 def get_media_by_id(media_id):
-    response = supabase.table('media').select('*').eq('id', media_id).execute()
+    response = supabase.table("media").select("*").eq("id", media_id).execute()
     return response
+
 
 def similarity_search(query):
     """
