c7f24ee363cabfaada5a39f18c2f6db4aff97ef0
vjz3qz <vjz3qz@virginia.edu>
2024-01-05 15:12:46 -0500

    created generate text wrapper interface for ai text service

--- app/services/ai_text_service.py
+++ app/services/ai_text_service.py
@@ -7,19 +7,25 @@ except KeyError:
     print('[error]: `API_KEY` environment variable required')
     sys.exit(1)
 
-
 api_key = os.environ.get('OPENAI_API_KEY')
 llm = ChatOpenAI(temperature=0, model_name="gpt-4-1106-preview",
                  openai_api_key=api_key)
 
+def generate_text(file, file_type):
+    if file_type == 'video':
+        return generate_video_transcript(file)
+    elif file_type == 'image':
+        return generate_image_summary(file)
+    return ""
+
 def generate_image_summary(image):
-    pass
+    return ""
 
 def generate_video_transcript(video):
-    pass
+    return ""
 
 def llm_inference(query, search_results):
 
     # calls openai endpoint to respond based on context
-    pass
+    return ""
 
