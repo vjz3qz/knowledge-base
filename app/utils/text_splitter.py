126e281979ed62a16300708b7997d5ec1253e474
vjz3qz <vjz3qz@virginia.edu>
2024-01-05 15:14:44 -0500

    got text from image/videos in text splitter

--- app/utils/text_splitter.py
+++ app/utils/text_splitter.py
@@ -1,13 +1,19 @@
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import S3FileLoader
+from app.services.blob_storage_service import get_text_file_path
+
+# Check for required environment variable
+try:  
+    bucket_name = os.environ['S3_BUCKET_NAME']
+except KeyError: 
+    raise EnvironmentError('[error]: `S3_BUCKET_NAME` environment variable required')
 
 def load_and_split(file_id, media_id, file_type):
     # load from s3
-    loader = S3FileLoader(media_id + "/" + file_type, file_id)
+    key = get_text_file_path(file_id, media_id, file_type)
+    loader = S3FileLoader(bucket_name, key)
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     return text_splitter.split_documents(documents)
 
 # aws_access_key_id="xxxx", aws_secret_access_key="yyyy" as params if not env vars
-
-# TODO ADD SUPPORT FOR IMAGE SUMMARIES AND VIDEO TRANSCRIPTS
